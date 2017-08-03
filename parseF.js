f21445b8522feff827982316b25d2b78c823e611
Alex <alexoh@wcoding.com>
2017-08-03 17:48:14 +0900

    Added zero test.

--- parseF.js
+++ parseF.js
@@ -11,3 +11,5 @@ console.log(parseF(false));     // This was added by me, on the local repository
 console.log(parseF(true));
 
 console.log(parseF(undefined)); /* This passes an undefined to the function. */
+
+console.log(parseF(0));
